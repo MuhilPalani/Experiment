1adf28e639a10cb96248a9505d9b80bba31cbd76
Muhil Palani <muhil156@gmail.com>
2025-12-07 14:37:24 +0100

    Update vite.config.ts
    base: "/Experiment/", //Set base for GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: "/Experiment/", //Set base for GitHub pages
     const env = loadEnv(mode, '.', '');
     return {
+      base: "/Experiment/", //Set base for GitHub pages
       server: {
         port: 3000,
         host: '0.0.0.0',
